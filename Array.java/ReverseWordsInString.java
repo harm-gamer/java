0542c083e6b0ee38016193552c5ff90bd34c31c5
harm-gamer <mukundt513@gmail.com>
2024-09-21 12:05:15 +0530

    reverse a word of String

---
+++ Array.java/ReverseWordsInString.java
@@ -0,0 +1,7 @@
+public class ReverseWordsInString{
+    public String reverseWords(String s) {
+        List<String> words = Arrays.asList(s.trim().split("\\s+"));
+        Collections.reverse(words);
+        return String.join(" ",words);
+    }
+}
