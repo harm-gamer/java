527176c45753970c9d88cfc198a3e064ea18a3a1
harm-gamer <mukundt513@gmail.com>
2024-09-22 11:08:02 +0530

    IndexofFirstOccurence

---
+++ Array.java/IndexofFirstOccurence.java
@@ -0,0 +1,20 @@
+public class IndexofFirstOccurence{
+    public static int firstOccurence(String haystack,String needle){
+        int haylength=haystack.length();
+        int needlelength=needle.length();
+        if(haylength<needlelength)
+            return -1;
+        for(int i=0;i<=haystack.length()-needle.length();i++){
+            int j=0;
+            while(j<needle.length() && haystack.charAt(i+j)==needle.charAt(j))
+                j++;
+            if(j==needle.length()){
+                return i;
+            }
+        }
+        return -1;
+            }
+    public static void main(String[] args){
+
+    }
+}
