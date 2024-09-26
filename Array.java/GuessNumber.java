3bca24b6acf624213ea34d171f78ef68a57d7720
harm-gamer <mukundt513@gmail.com>
2024-09-26 16:50:41 +0530

    merge sorted array

--- Array.java/GuessNumber.java
+++ Array.java/GuessNumber.java
@@ -1,19 +1,24 @@
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
 public class GuessNumber {
-    public int findMin(int[] nums) {
-        int ans =Integer.MAX_VALUE;int start = 0;
-        int end = nums.length-1;
-        
-        while(start<=end){
-            int mid = (start+end)/2;
-            
-            if(nums[start] <= nums[mid]){
-                ans = Math.min(nums[start],ans);
-                start = mid+1;
-            }else{
-                end = mid -1;
-                ans = Math.min(nums[mid],ans);
-            }
-        }
-        return ans;
-    }
+    public int guessNumber(int n) {
+        int start = 1;
+      int end = n;
+      
+      while(start<=end){
+          int mid = start + (end-start)/2;
+         int guessed = guess(mid);
+          
+          if(guessed == 0)return mid;
+          else if(guessed == -1) end = mid -1;
+          else start = mid +1;
+      }
+      return -1;
+  }
 }
