77d9522f0da93aba95f0fd667bc029b00e245aa5
harm-gamer <mukundt513@gmail.com>
2024-10-12 17:57:31 +0530

    Linked List Cycle

--- LinkedList.java/LinkedListCycle.java
+++ LinkedList.java/LinkedListCycle.java
@@ -1,26 +1,38 @@
 // Linked List Cycle
+// here is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+
 public class ListNode {
     int val;
-    ListNode next;  
-    ListNode() {}
-    ListNode(int val) { this.val = val; }
-    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
 }
+
 public class LinkedListCycle {
     public boolean hasCycle(ListNode head) {
-        if (head == null){
+        if (head == null) {
             return false;
         }
-         ListNode slow = head;
-         ListNode fast = head;
-         
-         while(fast != null && fast.next != null){
-              fast = fast.next.next;
-             slow = slow.next;
-             if(fast == slow){
-                 return true;
-             }
-         }
-         return false;
-     }
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
