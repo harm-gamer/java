69279a1b17a72c87a83c7a710d2b78fc89f0b1e5
harm-gamer <mukundt513@gmail.com>
2024-10-31 11:01:09 +0530

    Sum of Subarrays Range

---
+++ Amazon/Medium/SumofSubArraysRanges.java
@@ -0,0 +1,22 @@
+package Amazon.Medium;
+
+public class SumofSubArraysRanges{
+    public long subArrayRanges(int[] nums) {
+        int n = nums.length;
+        
+        long ans = 0;
+        
+        for(int i=0;i<n;i++){
+            int max = nums[i];
+            int min = nums[i];
+            for(int j =i;j<n;j++){
+                max = Math.max(max,nums[j]);
+                min = Math.min(min,nums[j]);
+                
+                ans += max-min;
+                
+            }
+        }
+        return ans;
+    }
+}
