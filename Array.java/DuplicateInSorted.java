da0cb79ce69e37009b49358dad86ad089d057930
harm-gamer <mukundt513@gmail.com>
2024-10-31 14:07:27 +0530

    Remove Duplicates from Sorted Array

---
+++ Array.java/DuplicateInSorted.java
@@ -0,0 +1,16 @@
+public class DuplicateInSorted {
+    public int removeDuplicates(int[] nums) {
+        
+        int n = nums.length;
+        
+        int k =1;
+        
+        for(int i=1;i<n;i++){
+            if(nums[i] != nums[k-1]){
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+}
