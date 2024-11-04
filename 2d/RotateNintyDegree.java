e0de1db04c6c740a04243ff9efd551d09a11ff0f
harm-gamer <mukundt513@gmail.com>
2024-11-04 21:04:42 +0530

    Roate image by 90 Degree

---
+++ 2d/RotateNintyDegree.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class RotateNintyDegree {
+    public void canRotate(int[][] matrix){
+    List<List<Integer>> list = new ArrayList<List<Integer>>();
+        
+    for(int i=0;i<matrix.length;i++){
+        List<Integer> l = new ArrayList<>();
+        for(int j= matrix.length -1;j>=0;j--){
+            l.add(matrix[j][i]);
+        }
+        list.add(l);
+    }
+    
+    for(int i=0;i<matrix.length;i++){
+        for(int j =0;j<matrix.length;j++){
+            matrix[i][j] = list.get(i).get(j);
+        }
+    }
+}
+}
