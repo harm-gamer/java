5aaf80232685dfe81a3aacbfa0979367e1fba8ea
harm-gamer <mukundt513@gmail.com>
2024-12-03 20:56:57 +0530

    Next Permutation

---
+++ MicroSoft/Medium/NextParmutation.java
@@ -0,0 +1,39 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int n = nums.length;
+        
+        // Step 1: Find the first decreasing element from the right
+        int i = n - 2;
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+        
+        if (i >= 0) {
+            // Step 2: Find the element just larger than nums[i]
+            int j = n - 1;
+            while (nums[j] <= nums[i]) {
+                j--;
+            }
+            // Step 3: Swap nums[i] and nums[j]
+            swap(nums, i, j);
+        }
+        
+        // Step 4: Reverse the suffix starting at nums[i + 1]
+        reverse(nums, i + 1, n - 1); 
+    }
+    
+    private static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    // Helper method to reverse a portion of the array
+    private static void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            swap(nums, start, end);
+            start++;
+            end--;
+        }
+    }
+}
