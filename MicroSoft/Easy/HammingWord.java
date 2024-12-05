a3d60bd3822173aa22c15cfe0abdb4ce0f03777b
harm-gamer <mukundt513@gmail.com>
2024-12-05 18:14:43 +0530

    Number of bits 1

---
+++ MicroSoft/Easy/HammingWord.java
@@ -0,0 +1,12 @@
+public class HammingWord{
+    public int hammingWeight(int n) {
+        int count = 0;
+        
+        while(n>0){
+            count += (n & 1);
+            n>>=1;
+        }
+        
+        return count;
+    }
+}
