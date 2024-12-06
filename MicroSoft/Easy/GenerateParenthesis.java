822423fba62a38498a6f325f72b7b54701619624
harm-gamer <mukundt513@gmail.com>
2024-12-06 17:27:43 +0530

    Generate Parenthesis

---
+++ MicroSoft/Easy/GenerateParenthesis.java
@@ -0,0 +1,23 @@
+public class GenerateParenthesis {
+    public void helper(List<String> ans,int Ocount,int Ccount,String str,int n){
+        if(Ocount == n && Ccount == n){
+            ans.add(str);
+        }
+        else{
+        if(Ocount>Ccount){
+            helper(ans,Ocount,Ccount+1,str+")",n);
+        }
+            if(Ocount<n){
+                helper(ans,Ocount+1,Ccount,str+"(",n);
+            }
+        }
+    }
+    public List<String> generateParenthesis(int n) {
+       List<String> ans = new ArrayList<>();
+        int openCount = 0;
+        int closeCount = 0;
+        String str = "";
+        helper(ans,openCount,closeCount,str,n);
+        return ans;
+    }
+}
